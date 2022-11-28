5d5f898de75e7bbd32c83aeabeb8ff991fd3b0e1
annatolchynska <anna.t1417@gmail.com>
2022-11-28 18:44:29 +0000

    Create a function starting the game.

--- run.py
+++ run.py
@@ -1,13 +1,29 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-# import random word from the list in words.txt
-import random
-from words.txt
+# Import random word from the list in words.py
+import random 
+from words import word_list
 
 def get_word():
     """
-    defines random word from the words in words.txt file
+    Defines random word from the words in words.py file
     """
-    word = random.choice(word.txt)
+    word = random.choice(word_list)
     return word
+
+def play(word):
+    """
+    The function running the game
+    """
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Welcome to Hangman game!\n")
+    print("You have to guess random word by one letter at a time\n")
+    print("You only have six tries otherwise you'll be hanged /n")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
